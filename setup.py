4e8503e976f7d398e654a85ba2a4b6bc5f2bc97a
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2018-07-06 14:43:12 +0200

    Bump version 0.6.3->0.6.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vydia',
-    version='0.6.3',
+    version='0.6.4',
 
     description='A modularized video player with resume function',
 
